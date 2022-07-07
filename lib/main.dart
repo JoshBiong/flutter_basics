1b360ded07870ca85549348683b678c3d8c25aee
Joshua Biong <joshuabiong7@gmail.com>
2022-07-07 21:01:48 +0800

    bottomnavigationbar onTap function

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,76 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 
-class MyApp extends StatelessWidget {
+
+
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+String buttomName = 'Joshua';
+int currentIndex = 0;
+
+
+
+
+  @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:  Scaffold(
+      home: Scaffold(
         appBar: AppBar(
-          title:const Center(child: Text('Flutter App')),
+          title: const Center(
+            child: Text('Flutter App'),
+          ),
         ),
-       body: const Center(child: Text('Body')),
-       bottomNavigationBar: BottomNavigationBar(items: const [
-
-        BottomNavigationBarItem(label: 'Home',
-          icon: Icon(Icons.home, color: Colors.blueAccent , size:30)),
-
-        BottomNavigationBarItem(label: 'Favorite',
-          icon: Icon(Icons.favorite, color: Colors.pink , size:30)),
-
-        BottomNavigationBarItem(label: 'Settings',
-          icon: Icon(Icons.settings, color: Colors.blueAccent , size:30)
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              setState(() {
+              if(buttomName == 'Joshua'){
+                buttomName= 'Jean Marie';
+              }else{
+                buttomName= 'Joshua';
+              }
+              });             
+            },
+            child: Text(buttomName),
+          ),
         ),
-       ],)
+
+        //bottom navigation bar
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+               label: 'Home',
+              icon: Icon(Icons.home,  size: 30),
+            ),
+            
+            BottomNavigationBarItem(
+              label: 'Settings',
+              icon: Icon(Icons.settings,size: 30),
+            ),
+          ],
+          currentIndex: currentIndex,
+          onTap: (int index){
+            setState(() {
+              currentIndex = index;
+            });
+
+          },
+          
         ),
-      );
-      
-    
+      ),
+    );
   }
 }
+
+
