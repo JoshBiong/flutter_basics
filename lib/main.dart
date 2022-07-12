2424ec32d31d28da3658e80aac58c99b6333115c
JoshBiong <joshuabiong7@gmail.com>
2022-07-13 00:06:01 +0800

    GestureDectector Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -11,75 +11,99 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: FirstPage(),
+      home: Homepage(),
     );
   }
 }
 
-class FirstPage extends StatelessWidget {
-  const FirstPage({
-    Key? key,
-  }) : super(key: key);
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  int currentIndex = 0;
+  bool _isImagechange = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Center(child: Text("First Page ")),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (BuildContext context) {
-                  return const SecondScreen();
-                },
-              ),
-            );
-          },
-          child: const Text('Click'),
+        title: const Center(
+          child: Text('First Page'),
         ),
+      ),
+      body: currentIndex == 0
+          ? Container(
+              decoration: const BoxDecoration(color: Colors.orangeAccent),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text('First Page'),
+                    ElevatedButton(
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (BuildContext context) {
+                            return const Secondpage();
+                          }));
+                        },
+                        child: const Text('Next Page'))
+                  ],
+                ),
+              ),
+            )
+          : GestureDetector(
+              onTap: () {
+                setState(() {
+                  _isImagechange = !_isImagechange;
+                });
+              },
+              child: _isImagechange
+                  ? Image.asset('image/anotherme.jpg')
+                  : Image.asset('iamge/me.jpg'),
+            ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            label: 'Home',
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            label: 'Settings',
+            icon: Icon(Icons.settings),
+          ),
+        ],
+        currentIndex: currentIndex,
+        onTap: (int index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
       ),
     );
   }
 }
 
-class SecondScreen extends StatelessWidget {
-  const SecondScreen({Key? key}) : super(key: key);
+class Secondpage extends StatefulWidget {
+  const Secondpage({super.key});
 
   @override
+  State<Secondpage> createState() => _SecondpageState();
+}
+
+class _SecondpageState extends State<Secondpage> {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Center(child: Text('Second Screen')),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Joshua Biong, Joshua Biong '),
-            const SizedBox(height: 10),
-            Center(
-              child: Center(
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).pop(
-                      MaterialPageRoute(
-                        builder: (BuildContext context) {
-                          return const FirstPage();
-                        },
-                      ),
-                    );
-                  },
-                  child: const Text('Back to first page'),
-                ),
-              ),
-            ),
-          ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text('Second Screen'),
         ),
       ),
+      body: Image.asset('image/me.jpg'),
     );
   }
 }
